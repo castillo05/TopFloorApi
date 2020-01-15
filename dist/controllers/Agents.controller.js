d900a645754424bac5ca653567fd29c9baa8f093
castillo05 <jorgeantonio20142014@gmail.com>
2020-01-15 14:14:30 -0600

    Implementando async-await y try catch

--- dist/controllers/Agents.controller.js
+++ dist/controllers/Agents.controller.js
@@ -6,64 +6,65 @@ var _Agent = _interopRequireDefault(require("../modelsNoSql/Agent.model"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-let getAgents = (req, res) => {
-  _models.default.agents.findAll({}).then(agents => {
-    res.status(200).send({
-      agents: agents
-    });
-    agents.forEach(element => {
-      console.log(element.AGENT_CODE);
-
-      _Agent.default.findOne({
-        agentCode: element.AGENT_CODE
-      }, (err, resul) => {
-        if (err) return res.status(500).send({
-          message: err
-        });
-
-        if (!resul) {
-          console.log({
-            mesagge: 'Insertando datos'
-          });
-          let A = new _Agent.default();
-          A.agentCode = element.AGENT_CODE;
-          A.agentName = element.AGENT_NAME;
-          A.agentCommission = element.COMMISSION;
-          A.save((err, result) => {
-            if (err) return res.status(500).send({
-              message: err
-            });
-            console.log(result);
-          });
-        } else {
-          return console.log({
-            mesagge: 'No hay nuevos datos'
-          });
-        }
-      });
-    });
-  }).catch(err => {
-    res.status(500).send({
-      message: err
-    });
-  });
-};
-
-async function getInformationOfAgents(req, res) {
-  let query = _Agent.default.find();
-
-  await query.exec((err, agents) => {
-    if (err) {
-      res.status(500).send({
-        message: err
-      });
-    } else {
+let getAgents = async (req, res) => {
+  try {
+    await _models.default.agents.findAll({}).then(agents => {
       res.status(200).send({
         agents: agents
       });
-    }
-  });
-}
+      agents.forEach(element => {
+        // console.log(element.AGENT_CODE);
+        _Agent.default.findOne({
+          agentCode: element.AGENT_CODE
+        }, (err, resul) => {
+          if (err) return res.status(500).send({
+            message: err
+          });
+
+          if (!resul) {
+            let A = new _Agent.default();
+            A.agentCode = element.AGENT_CODE;
+            A.agentName = element.AGENT_NAME;
+            A.agentCommission = element.COMMISSION;
+            A.save((err, result) => {
+              if (err) return res.status(500).send({
+                message: err
+              });
+              console.log(result);
+            });
+          } else {// return console.log({mesagge:'No hay nuevos datos'});
+          }
+        });
+      });
+    }).catch(err => {
+      res.status(500).send({
+        message: err
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getInformationOfAgents = async (req, res) => {
+  try {
+    let query = _Agent.default.find();
+
+    await query.exec((err, agents) => {
+      if (err) {
+        res.status(500).send({
+          message: err
+        });
+      } else {
+        res.status(200).send({
+          agents: agents
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   getAgents,
--- src/controllers/Agents.controller.js
+++ src/controllers/Agents.controller.js
@@ -3,56 +3,55 @@
 import db from '../models';
 import Agents from '../modelsNoSql/Agent.model';
 
-let getAgents=(req,res)=>{
-    db.agents.findAll({
-
-    }).then(agents=>{
-        res.status(200).send({agents:agents});
-
-        agents.forEach(element => {
-            console.log(element.AGENT_CODE);
-
-            Agents.findOne({agentCode:element.AGENT_CODE},(err,resul)=>{
-                if(err) return res.status(500).send({message:err});
-
-                if(!resul){
-                    console.log({mesagge:'Insertando datos'});
-                     let A = new Agents();
-                    A.agentCode=element.AGENT_CODE;
-                    A.agentName=element.AGENT_NAME;
-                    A.agentCommission=element.COMMISSION
-
-                    A.save((err,result)=>{
-                        if(err) return res.status(500).send({message:err});
-
-                        console.log(result);
-                    });
-                }else{
-                    return console.log({mesagge:'No hay nuevos datos'});
-                }
+let getAgents= async (req,res)=>{
+    try {
+        await db.agents.findAll({}).then(agents=>{
+            res.status(200).send({agents:agents});
+            agents.forEach(element => {
+                // console.log(element.AGENT_CODE);
+                Agents.findOne({agentCode:element.AGENT_CODE},(err,resul)=>{
+                    if(err) return res.status(500).send({message:err});
+    
+                    if(!resul){
+                       
+                         let A = new Agents();
+                        A.agentCode=element.AGENT_CODE;
+                        A.agentName=element.AGENT_NAME;
+                        A.agentCommission=element.COMMISSION
+    
+                        A.save((err,result)=>{
+                            if(err) return res.status(500).send({message:err});
+                            console.log(result);
+                        });
+                    }else{
+                        // return console.log({mesagge:'No hay nuevos datos'});
+                    }
+                });
             });
-           
+        }).catch(err=>{ 
+            res.status(500).send({message:err});
         });
-        
-
-
-
-    }).catch(err=>{ 
-        res.status(500).send({message:err});
-    });
+    } catch (error) {
+        console.log(error);
+    }
+   
 }
 
-async function getInformationOfAgents(req, res){
+const getInformationOfAgents=async(req, res)=>{
+    try{
+        let query = Agents.find();
 
-    let query = Agents.find();
-
-   await query.exec((err,agents)=>{
-        if(err){
-        res.status(500).send({message:err});
-        }else{
-            res.status(200).send({agents:agents});
-        }
-    });
+        await query.exec((err,agents)=>{
+                if(err){
+                res.status(500).send({message:err});
+                }else{
+                    res.status(200).send({agents:agents});
+                }
+            });
+    }catch(error){
+        console.log(error);
+    }
+    
 
 }
 
